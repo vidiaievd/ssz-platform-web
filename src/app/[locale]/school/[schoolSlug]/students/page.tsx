e682b3758837f3aa6c34e571a209a950f1719028
Dmytro Vidiaiev <vidiaievdmytro@gmail.com>
2026-06-07 20:00:53 +0200

    fixed lint error

--- src/app/[locale]/school/[schoolSlug]/students/page.tsx
+++ src/app/[locale]/school/[schoolSlug]/students/page.tsx
@@ -123,7 +123,7 @@ export default async function SchoolStudentsPage({ params, searchParams }: Props
           schoolSlug={schoolSlug}
           activeSegment={activeSegment}
           search={q ?? ''}
-          onEnroll={() => {}}
+          onEnrollAction={() => {}}
         />
       )}
     </main>
